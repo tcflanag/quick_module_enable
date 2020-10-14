36afc960c22c4cc9ebeca1ac221e5cd28d2b94d9
Chris Flanagan <tiwato@gmail.com>
2020-10-14 16:55:54 -0700

    Cleanup, and release prep

---
+++ README.md
@@ -0,0 +1,7 @@
+# Quick Module Enable
+
+This module allows you to enable newly installed modules with a single click. Upon loading a world, if there are any new modules since the last load,it will display the Module Manager with a filter showing just the new modules, which have been pre-checked for enabling. You will simply need to click the 'Save' button at the bottom. You can uncheck any modules you don't want installed, or simply click 'X' to make no changes.
+
+In addition, it provides a 'Recent' filter to the stock Module Manager which shows modules that have been recently installed, or updated.
+
+![Example of module manager with recent filter in selected](example.png)
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ Hooks.once("ready", quick_enable_init)
 async function quick_enable_init() {
     if (!game.user.isGM) return
     const history_size = 10
-    var modVer = {};
-
-    for (var mod of game.data.modules) modVer[mod.id] = {version: mod.data.version}
     
     game.settings.register('quick-module-enable', "previousModules", {
         scope: "world",
@@ -14,56 +11,85 @@ async function quick_enable_init() {
         config: false,
         onChange: s => {}
     });    
-     
+
+    //Get current mod list
+    var modVer = {}
+    for (var mod of game.data.modules) modVer[mod.id] = {version: mod.data.version}
+
+    //Get mod history
     var modHistory = game.settings.get('quick-module-enable','previousModules')
 
+    // Initialize history if empty
     if (modHistory.length === 0) modHistory.push(modVer)
     while (modHistory.length <= history_size) modHistory.unshift(modHistory[0])
     while (modHistory.length > history_size) modHistory.shift()
 
+    // Update history
     modHistory.shift() // Delete the oldest data
     modHistory.push(modVer) // Add this runs data to the stack
     await game.settings.set('quick-module-enable','previousModules',modHistory)
 
+    // Monkeypatch to reuse the existing Modmanagment API
     ModuleManagement.prototype.realGetData = ModuleManagement.prototype.getData
     ModuleManagement.prototype.getData = getQuickEnableData
 
+    // Check if there are any new mods, and display the manager if so
     var oldVer = modHistory.slice(-2)[0] // Second to last elemet is state at previous load
+    var changes = false
     for (mod of Object.keys(modVer)) {
         if (!(mod in oldVer)){
-            var b = new ModuleManagement();
-            b._filter = "recent"
-            b.render(true);   
-        } 
+            changes = true
+            console.log("quick-module-enable - ",mod," added version ",modVer[mod]["version"])
+        }
         if (mod in modHistory[0] && modVer[mod]["version"] !== modHistory[0][mod]["version"]){
             console.log("quick-module-enable - ",mod," version change from ",modHistory[0][mod]["version"]," to ",modVer[mod]["version"])
         }
     }   
+    if (changes) {
+        var b = new ModuleManagement();
+        b._filter = "recent"  // Default the dissplay to the "Recent" tb
+        b._quick_install_mode = true // Auto enable new mods
+        b.render(true);   
+    }
 }
 
 
 function getQuickEnableData(options) {
-    var data =this.realGetData(options)
+    var data = this.realGetData(options) // Don't want to copy the bulk of this function for compatability.
     var counts_recent = 0    
     const modVer = game.settings.get('quick-module-enable','previousModules')[0] // Element 0 is oldest, so check it for version
     const newMod = game.settings.get('quick-module-enable','previousModules').slice(-2)[0] // Second to last elemet is state at previous load
     
-    data.modules = data.modules.reduce((arr, m) => {
+    // Count loop is seperate from filter loop so that count is always correct
+    // Othewise, since I'm using the output of the real GetData function (above), the count would change depending on those filters too
+    for (var m of game.data.modules){
 
-        var isNew =!(m.name in newMod)
-        var isUpdated =!(m.name in modVer && modVer[m.name]["version"] === m.version)
-        
+        var isNew =!(m.data.name in newMod)
+        var isUpdated =!(m.data.name in modVer && modVer[m.data.name]["version"] === m.data.version)
         if (isUpdated || isNew) {
             counts_recent++;
-            modVer[m.name] = {"version":m.version}
         }
-                
-        if (this._filter === "recent" && !(isUpdated||isNew)) return arr;          
-        if (this._filter === "recent" && isNew) m.active = true
+    }
 
-        return arr.concat([m]);
-    }, []);
+    // Filter the list when "recent" is chosen to just have new or updated
+    // Pre-check the new mods if this is the startup display
+    if (this._filter === "recent") {
+        data.modules = data.modules.reduce((arr, m) => {
 
+            var isNew =!(m.name in newMod)
+            var isUpdated =!(m.name in modVer && modVer[m.name]["version"] === m.version)
+                    
+            if (!(isUpdated||isNew)) return arr;          
+            if (isNew && this._quick_install_mode) m.active = true
+
+            return arr.concat([m]);
+        }, []);
+    }
+
+    // Only show "Recent" on the startup display
+    //if (this._quick_install_mode) data["filters"] = []
+
+    // Add a filter to the ModuleManagment page.
     data["filters"].push({id:"recent",
                             label: game.i18n.localize('QUICKMODMANAGE.FilterRecent'),
                             css: this._filter === "recent" ? " active" : "",
